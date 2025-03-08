1ce4400ea3902a7aa8d5e2f7056fff7d009319cc
matu.dev <matu.a.gimenez2002@gmail.com>
2025-03-07 22:44:21 -0300

    clase 3

--- Clase-2-Nivelacion-JS/script.js
+++ Clase-2-Nivelacion-JS/script.js
@@ -85,3 +85,56 @@ else{
 }
  */
 
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+    const input = document.querySelector("#input-note");
+    const notesContainer = document.querySelector("#notes-container");
+    
+    //Estado de las notas
+    let notes = [];
+    
+    function generateId() {
+        return '_' + Math.random().toString(36).substr(2, 9);
+    }
+    
+    function renderNotes() {
+        notesContainer.innerHTML = "";
+        notes.forEach(note => {
+            const noteElement = document.createElement("div");
+            
+            //Es una decision mia
+            noteElement.dataset.id = note.id;
+            
+            const span = document.createElement("span");
+            span.textContent = note.text;
+            
+            const button = document.createElement("button");
+            button.classList.add("delete-btn");
+            button.innerHTML = '<i class="bi bi-trash3"></i>';
+            button.addEventListener("click", () => deleteNote(note.id));
+            
+            noteElement.appendChild(span);
+            noteElement.appendChild(button);
+            notesContainer.appendChild(noteElement);
+        });
+    }
+    
+    function deleteNote(id) {
+        notes = notes.filter(note => note.id !== id);
+        renderNotes();
+    }
+    
+    form.addEventListener("submit", (event) => {
+        //Prevenimos el comportamiento por defecto del formulario (osea que no se recargue la pagina)
+        event.preventDefault();
+        const noteText = input.value.trim();
+        if (noteText) {
+            // NO seria correcto manipular el HTML aca
+            notes.push({ id: generateId(), text: noteText });
+            input.value = "";
+            renderNotes();
+        }
+    });
+    
+    renderNotes();
+});
