cc508c39bc4f24f71702c64bbf7558fe8cae5ef4
matu.dev <mati.a.gimenez2002@gmail.com>
2025-04-11 21:06:38 -0300

    clase 8 React Router DOM

---
+++ clase-8/src/Screens/ContactScreen/ContactScreen.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+
+const ContactScreen = () => {
+  return (
+    <div>
+        <Navbar/>
+        <h1>Contactanos</h1>
+        <form>
+            {/* Sigan con este form */}
+        </form>
+    </div>
+  )
+}
+
+export default ContactScreen
