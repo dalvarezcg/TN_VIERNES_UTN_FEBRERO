b694a922457275631c88b75cf0af0267ee6d8fa5
matu.dev <mati.a.gimenez2002@gmail.com>
2025-05-16 19:54:00 -0300

    Formulario de productos

--- clase-11 Firebase/src/Screens/CreateProductScreen/CreateProductScreen.jsx
+++ clase-11 Firebase/src/Screens/CreateProductScreen/CreateProductScreen.jsx
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateProductScreen = () => {
+    //Cada vez que cambie el valor de un input voy a actualizar mi estado de formulario
+    let initial_state_form = {
+        title: '',
+        price: 0,
+        discount: 0,
+        img: null
+    }
+    const [form_state, setFormState] = useState(initial_state_form)
+    
+    const handleChange = (event) => {
+        let field = event.target.name
+        let new_value = event.target.value
+        setFormState(
+            (prev_state) => {
+                return {
+                    ...prev_state,
+                    [field]: new_value
+                }
+            }
+        )
+    }
+
+    console.log(form_state)
     return (
         <div>
             <h1>Crea tu producto</h1>
@@ -12,6 +35,8 @@ const CreateProductScreen = () => {
                         name='title' 
                         id="title" 
                         placeholder='Escribe el titulo...' 
+                        value={form_state.title}
+                        onChange={handleChange}
                         required
                     />
                 </div>
@@ -23,6 +48,8 @@ const CreateProductScreen = () => {
                         id="price" 
                         placeholder='Escribe el precio...' 
                         min={0} 
+                        value={form_state.price}
+                        onChange={handleChange}
                         required
                     />
                 </div>
@@ -33,13 +60,20 @@ const CreateProductScreen = () => {
                         name='discount' 
                         id="discount" 
                         placeholder='Escribe el descuento...' 
+                        max={99}
                         min={0} 
+                        value={form_state.discount}
+                        onChange={handleChange}
                         required
                     />
                 </div>
                 <div>
                     <label htmlFor="img">Seleciona una imagen:</label>
-                    <input type='file' id='img' name='img' />
+                    <input 
+                        type='file' 
+                        id='img' 
+                        name='img' 
+                    />
                 </div>
                 <button 
                     type='submit'
